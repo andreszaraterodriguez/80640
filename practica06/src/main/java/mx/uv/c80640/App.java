b4fefa80bafe8d3cce403463d3b39999c9f65f91
andreszaraterodriguez <andres.zarate162ver@gmail.com>
2022-10-20 12:56:54 -0500

    web

--- practica06/src/main/java/mx/uv/c80640/App.java
+++ practica06/src/main/java/mx/uv/c80640/App.java
@@ -1,13 +1,15 @@
 package mx.uv.c80640;
+import static spark.Spark.*;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        get("/andy", (req, res) -> "Hola andy");
+        get("/sara", (req, res) -> "Hola sara");
+        get("/", (req, res) -> "<h1>Bienbenida</h1><img src='https://www.uv.mx/v2/images/logouv.jpg'> "
+        
+        );
     }
 }
