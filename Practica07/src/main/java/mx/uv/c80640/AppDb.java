30edfb802b9d0835f2f2c0c84f91e82c6a3d38af
andreszaraterodriguez <andres.zarate162ver@gmail.com>
2022-11-10 13:52:20 -0600

    class

---
+++ Practica07/src/main/java/mx/uv/c80640/AppDb.java
@@ -0,0 +1,40 @@
+package mx.uv.c80640;
+
+import static spark.Spark.*;
+import java.util.Map;
+import com.google.gson.Gson;
+import java.util.HashMap;
+
+/**
+ * Hello world!
+ *
+ */
+public class AppDb {
+    public static Gson gson = new Gson();
+    //base de datos en memoria
+    public static Map<String, Usuario> usuarios = new HashMap<>();
+    public static void main( String[] args )
+    {
+        port(80);
+        //inicializacion de datos
+        Usuario u1 = new Usuario("1","samuel", "123456");
+        Usuario u2 = new Usuario("2","alan", "42347");
+        usuarios.put(u1.getId(),u1);
+        usuarios.put(u2.getId(),u2);
+
+
+        System.out.println( "Hello World!" );
+        //before ((req, res)->res.type("aplication/json"));
+        get("/usuario",(req,res) -> gson.toJson(u1));
+        get("/usuarios",(req,res) -> gson.toJson(usuarios));
+
+        post("/", (req, res)->{
+            String datosFormulario = req.body();
+            Usuario u = gson.fromJson(datosFormulario, Usuario.class);
+            usuarios.put(u.getId(),u);
+            return "usuario agregado";
+        });
+
+    }
+
+}
